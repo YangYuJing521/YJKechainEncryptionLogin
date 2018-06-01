e69e691d96cedc4cdcdbf16c010b6d6c45f61b8d
YangYuJing521 <475201052@qq.com>
2018-06-01 10:17:13 +0800

    添加UI

--- YJKechainEncryptionLogin/YJKechainEncryptionLogin/ViewController.m
+++ YJKechainEncryptionLogin/YJKechainEncryptionLogin/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userNameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -16,13 +18,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)loginBtnClick:(UIButton *)sender {
+    
+    NSLog(@"longinClick");
 }
 
 
