a962eb0d031ff248688ec8468577cadf42fe1b1e
YangYuJing521 <475201052@qq.com>
2018-12-18 12:00:43 +0800

    no message

--- YJKechainEncryptionLogin/YJKechainEncryptionLogin/ViewController.m
+++ YJKechainEncryptionLogin/YJKechainEncryptionLogin/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "MDProgressHUD.h"
 #import "YJLoginHandler.h"
-
+#import <LocalAuthentication/LocalAuthentication.h>
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *userNameField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
@@ -40,7 +40,6 @@
         }
     }];
 }
-
 
 #pragma mark 点击事件
 /** 登录按钮点击事件 */
@@ -74,6 +73,54 @@
 - (IBAction)autoLoginClick:(UISwitch *)sender {
     [[NSUserDefaults standardUserDefaults] setBool:sender.isOn forKey:autoLoginDefaults];
     [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+- (IBAction)zhiWenAuth:(UIButton *)sender {
+    if ([UIDevice currentDevice].systemVersion.floatValue>=8.0) {//ios8.0以后支持
+        LAContext *contex = [[LAContext alloc] init];
+        //LAPolicyDeviceOwnerAuthentication  //2次失败后弹出密码验证
+        //LAPolicyDeviceOwnerAuthenticationWithBiometrics  3次+2次没通过 不会再验证
+        if ([contex canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:nil]) {//iphone 5s以后支持
+            
+            //执行验证
+            [contex evaluatePolicy:LAPolicyDeviceOwnerAuthentication localizedReason:@"开启指纹验证以开启高级功能" reply:^(BOOL success, NSError * _Nullable error) {
+                if (success) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [self ShowText:@"验证成功"];
+                    });
+                }else{
+                    
+                    /*
+                     LAErrorAuthenticationFailed - 指纹无法识别
+                     LAErrorUserCancel     --用户点击了取消
+                     LAErrorUserFallback   --用户点击了输入密码
+                     LAErrorSystemCancel   --系统取消
+                     LAErrorPasscodeNotSet --因为你设备上没有设置密码
+                     LAErrorTouchIDNotAvailable  --设备没有Touch ID
+                     LAErrorTouchIDNotEnrolled   --因为你的用户没有输入指纹
+                     LAErrorTouchIDLockout --多次输入，密码锁定
+                     LAErrorAppCancel--    比如电话进入，用户不可控的
+                     */
+                    
+                    switch (error.code) {
+                        case LAErrorAuthenticationFailed:{
+                            dispatch_async(dispatch_get_main_queue(), ^{
+                                [self ShowText:@"指纹无法识别"];
+                            });
+                            
+                        }
+                            break;
+                            
+                        default:
+                            break;
+                    }
+                }
+            }];
+            
+        }else{
+            [self ShowText:@"不支持指纹识别"];
+        }
+    }
 }
 
 #pragma mark 私有方法
